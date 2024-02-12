c24d7c3811643ce9e521f97909deac6fb3775d5d
cyrus-estavillo <cyrus.estavillo@gmail.com>
2024-02-11 22:51:06 -0500

    Adding some files. No code

---
+++ client/src/pages/SignUp.jsx
@@ -0,0 +1,11 @@
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+
+import React from 'react'
+
+export default function SignUp() {
+  return (
+    <div>
+      
+    </div>
+  )
+}

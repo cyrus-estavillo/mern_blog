708dd7b6d10ef0ad9d33d1e95f15e170f32c8f38
cyrus-estavillo <cyrus.estavillo@gmail.com>
2024-02-14 15:20:39 -0500

    Completed sidebar of the dashboard

---
+++ client/src/components/DashSidebar.jsx
@@ -0,0 +1,36 @@
+import { Sidebar } from 'flowbite-react';
+import { HiUser, HiArrowSmRight } from 'react-icons/hi';
+import { Link, useLocation } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+
+export default function DashSidebar() {
+    const location = useLocation();
+    const [tab, setTab] = useState('');
+
+    useEffect(() => {
+        const urlParams = new URLSearchParams(location.search); // location.search is the query string in the URL
+        const tabFromUrl = urlParams.get('tab'); // get the value of the 'tab' query parameter aka www.example.com?tab=profile
+        // console.log(tabFromUrl); 
+        if (tabFromUrl) {
+        setTab(tabFromUrl);
+        }
+    }, [location.search]);
+
+  return (
+    <Sidebar className="w-full md:w-56">
+        <Sidebar.Items>
+            <Sidebar.ItemGroup>
+                <Link to='/dashboard?tab=profile'>
+                    <Sidebar.Item active={tab === 'profile'} icon={HiUser} label={"User"} labelColor="dark">
+                        Profile
+                    </Sidebar.Item>
+                </Link>
+
+                <Sidebar.Item icon={HiArrowSmRight} className="cursor-pointer">
+                    Sign Out
+                </Sidebar.Item>
+            </Sidebar.ItemGroup>
+        </Sidebar.Items>
+    </Sidebar>
+  )
+}
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,9 +1,30 @@
-
+import { useLocation } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import DashSidebar from '../components/DashSidebar';
+import DashProfile from '../components/DashProfile';
 
 export default function Dashboard() {
+  const location = useLocation();
+  const [tab, setTab] = useState('');
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search); // location.search is the query string in the URL
+    const tabFromUrl = urlParams.get('tab'); // get the value of the 'tab' query parameter aka www.example.com?tab=profile
+    // console.log(tabFromUrl); 
+    if (tabFromUrl) {
+      setTab(tabFromUrl);
+    }
+  }, [location.search]);
+
   return (
-    <div>
-      Dashboard
+    <div className="min-h-screen flex flex-col md:flex-row">
+      <div className = "md:w-56">
+        {/* Sidebar */}
+        <DashSidebar />
+      </div>
+        {/* Profile and Et Cetera*/}
+        {tab === 'profile' && <DashProfile />}
+      
     </div>
   )
 }

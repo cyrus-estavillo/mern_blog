bbfb2977bd4fc2566be4fc6fd56e744ca12f3c75
cyrus-estavillo <cyrus.estavillo@gmail.com>
2024-02-18 14:15:20 -0500

    Added delete user functionality to the admin user

--- client/src/components/DashUsers.jsx
+++ client/src/components/DashUsers.jsx
@@ -49,7 +49,23 @@ export default function DashUsers() {
   };
 
 
-  const handleDeleteUser = async () => {};
+  const handleDeleteUser = async () => {
+    try {
+        const res = await fetch(`/server/user/delete/${userIdToDelete}`, {
+            method: 'DELETE',
+        });
+        const data = await res.json();
+        if (res.ok) {
+            setUsers((prev) => prev.filter((user) => user._id !== userIdToDelete));
+            setShowModal(false);
+        } else {
+            console.log(data.message);
+        }
+
+    } catch (error) {
+        console.log(error.message);
+    }
+  };
 
 
   return (

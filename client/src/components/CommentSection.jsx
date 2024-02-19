294b95247468f1b9110509cc92c6a6a13fc554f2
cyrus-estavillo <cyrus.estavillo@gmail.com>
2024-02-19 02:01:42 -0500

    Added edit functionality to the comment component

--- client/src/components/CommentSection.jsx
+++ client/src/components/CommentSection.jsx
@@ -89,6 +89,13 @@ export default function CommentSection({ postId }) {
   };
 
 
+  const handleEdit = async (comment, editedContent) => {
+    setComments(
+      comments.map((c) => 
+        c._id === comment._id ? { ...c, content: editedContent } : c))
+  };
+
+
   return (
     <div className='max-w-3xl mx-auto w-full p-3'>
       {currentUser ? 
@@ -147,7 +154,7 @@ export default function CommentSection({ postId }) {
               </div>
             </div>
             {comments.map((comment) => (
-              <Comment key={comment._id} comment={comment} onLike={handleLike}/>
+              <Comment key={comment._id} comment={comment} onLike={handleLike} onEdit={handleEdit}/>
             ))}
           </>
           

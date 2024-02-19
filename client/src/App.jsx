df5bdf352c052debd1e86079b28fb8636a4b0df5
cyrus-estavillo <cyrus.estavillo@gmail.com>
2024-02-18 21:01:06 -0500

    Added comment section to the post page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,11 +11,13 @@ import PrivateRoute from './components/PrivateRoute';
 import OnlyAdminPrivateRoute from './components/OnlyAdminPrivateRoute';
 import CreatePost from './pages/CreatePost';
 import UpdatePost from './pages/UpdatePost';
-import PostPage from './components/PostPage';
+import PostPage from './pages/PostPage';
+import ScrollToTop from './components/ScrollToTop';
 
 export default function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Header />
       
       <Routes>
